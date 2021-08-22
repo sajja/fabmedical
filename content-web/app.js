699dcde60e52c1a4e991835d1ae47c0476321069
Ubuntu <sajith@fabmedical6-pol.vgai2rneuusuvkrk3p5w04khdd.bx.internal.cloudapp.net>
2021-08-22 15:50:28 +0000

    x

--- content-web/app.js
+++ content-web/app.js
@@ -6,7 +6,7 @@ const request = require('request');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
-const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
+const contentApiUrl = process.env.CONTENT_API_URL || "http://40.114.53.131:3001";
 
 
 function getSessions(cb) {
@@ -21,6 +21,8 @@ function getSessions(cb) {
 
 function getSpeakers(cb) {
   request(contentApiUrl + '/speakers', function (err, response, body) {
+    console.log("woo hoo");
+
     if (err) {
       return cb(err);
     }
